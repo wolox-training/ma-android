257ac69c1ab6aebef82ee035efd37fcfa1227927
mariaAcevedo <maria.acevedo@wolox.com.ar>
2019-06-05 11:32:38 -0300

    All three backend classes are finished

--- app/src/main/java/ar/com/wolox/android/example/network/UserService.java
+++ app/src/main/java/ar/com/wolox/android/example/network/UserService.java
@@ -2,7 +2,7 @@ package ar.com.wolox.android.example.network;
 
 import java.util.List;
 
-import ar.com.wolox.android.example.API.User;
+import ar.com.wolox.android.example.model.User;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -11,8 +11,11 @@ import retrofit2.http.Query;
  * User service - Interface used with retrofit
  */
 public interface UserService {
+    String USERS_COLLECTION = "/users";
+    String EMAIL = "email";
+    String PASSWORD = "password";
 
-    @GET("/users")
-    Call<List<User>> getUserByMail(@Query("email") String mail, @Query("password") String password);
+    @GET(USERS_COLLECTION)
+    Call<List<User>> getUserByMail(@Query(EMAIL) String mail, @Query(PASSWORD) String password);
 
 }
--- app/src/main/java/ar/com/wolox/android/example/ui/login/ILoginView.java
+++ app/src/main/java/ar/com/wolox/android/example/ui/login/ILoginView.java
@@ -6,6 +6,7 @@ public interface ILoginView {
     /*If userFound value is true, there is an user already signed up with the email rpovided, otherwise, there is not
         If password value is true, then the password entered is correct. Otherwise, this value is false.
     */
-    void onGetUserByMailFinished(Boolean userFound);
+    void onGetUserByMailFinished(Boolean userIsFound);
     void cellphoneIsDisconnecteed();
+    void failedApiConnection();
 }
